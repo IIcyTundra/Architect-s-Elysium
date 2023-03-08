74c7cd3f8673f92b355414acbbf50411bda5a882
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2023-03-08 14:44:20 -0600

    3D Test Created

--- Assets/Weapons/Weapons_ScObj.cs
+++ Assets/Weapons/Weapons_ScObj.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Weapons", menuName = "Create Weapon")]
+[CreateAssetMenu(fileName = "Weapons", menuName = "Create Scriptable Object/Weapon")]
 public class Weapons_ScObj : ScriptableObject
 {
-    public GameObject Weapon_Obj;
-    public SpriteRenderer Weapon_Sprite; 
+    public Sprite WeaponSprite;
+    [Header ("Weapon Stats")]
     public float Dmg = 100;
     public float RMP = 50;
     public float Magazine = 12;
     public float Ammo_Max = 300;
-    public string Element_Type = "Kinetic";
+    public string Element_Type;
 }
