d6e863f3acf6594443b88a3ded21239d79773e63
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2023-03-28 13:59:35 -0500

    quick fix

--- Assets/Weapons/Scripts/Hellwalker_SO.cs
+++ Assets/Weapons/Scripts/Hellwalker_SO.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "HellWalker", menuName = "Create Scriptable Object/HellWalker")]
 public class Hellwalker_SO : ScriptableObject
 {
-    public Sprite HellwalkerSprite;
-    public string HellwalkerName = "Hellwalker";
-    public int HellwalkerID = 1;
-
-    public 
+    public GameObject HW_Obj;
+    public Sprite HW_Sprite;
+    public string HW_Name = "Hellwalker";
+    public int HW_ID = 1;
+    public float HW_Range = 5f;
+    public int HW_Ammo_Max = 40;
+    public float HW_Spread = 0.9f;
 }
