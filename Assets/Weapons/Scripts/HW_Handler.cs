fcbc317f1006d857ecb41c05d28db481d46c3d43
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2023-03-29 18:23:53 -0500

    Updated weapons

---
+++ Assets/Weapons/Scripts/HW_Handler.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using TMPro;
+
+public class HW_Handler : MonoBehaviour
+{
+    //Gun stats
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools 
+    bool shooting, readyToShoot, reloading;
+
+    //Reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask Enemy;
+
+    //Graphics
+    //public GameObject muzzleFlash, bulletHoleGraphic;
+    //public CamShake camShake;
+    //public float camShakeMagnitude, camShakeDuration;
+    public TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+        
+
+        //SetText
+        text.SetText(bulletsLeft + "/" + magazineSize);
+    }
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        //Shoot
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0){
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        Debug.DrawRay(fpsCam.transform.position, fpsCam.transform.forward * range, Color.green);
+        readyToShoot = false;
+
+        //Spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate Direction with Spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+        
+        //RayCast
+        if (Physics.Raycast(attackPoint.transform.position, direction, out rayHit, range, Enemy))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            // if (rayHit.collider.CompareTag("Enemy"))
+            //     rayHit.collider.GetComponent<ShootingAi>().TakeDamage(damage);
+        }
+
+
+        //Graphics
+        // Instantiate(bulletHoleGraphic, rayHit.point , Quaternion.FromToRotation(Vector3.forward, rayHit.normal));
+        // Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if(bulletsShot > 0 && bulletsLeft > 0)
+        Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    } 
+}  
--- Assets/Weapons/Scripts/Hellwalker_SO.cs
+++ Assets/Weapons/Scripts/Hellwalker_SO.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HellWalker", menuName = "Create Scriptable Object/HellWalker")]
+[CreateAssetMenu(fileName = "Hellwalker", menuName = "Create Scriptable Object/HellWalker")]
 public class Hellwalker_SO : ScriptableObject
 {
     public GameObject HW_Obj;
-    public Sprite HW_Sprite;
     public string HW_Name = "Hellwalker";
     public int HW_ID = 1;
     public float HW_Range = 5f;
-    public int HW_Ammo_Max = 40;
-    public float HW_Spread = 0.9f;
+    public int HW_Ammo_Max = 8;
+    public float HW_SpreadAngle = 20f; //Cone Angle
+    public float HW_Radius = 10f; //Cone Radius
 }
